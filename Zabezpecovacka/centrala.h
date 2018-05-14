984e06fad64f49db5d8f1b5c116436aea2f5529a
David <machacek.davi@seznam.cz>
2018-05-14 09:24:54 +0200

    senzory hotovy , priprava pole

--- Zabezpecovacka/centrala.h
+++ Zabezpecovacka/centrala.h
@@ -1,1 +1,56 @@
 #pragma once
+
+enum  d_budova { VSE, DUM, GARAZ };
+enum  d_typsenzor {kontaktni, pohybovy, co2, pozarni};
+
+class base
+{
+public:
+	base();
+	~base();
+
+protected:
+	int pocetsenzoru;
+	bool zamceno;
+	bool poplach;
+};
+
+class domov: public base
+{
+public:
+	domov();
+	~domov();
+	virtual void zamkni();
+
+protected:
+
+};
+
+class garaz: public base
+{
+public:
+	garaz();
+	~garaz();
+	virtual void zamkni();
+
+private:
+
+};
+
+
+class centrala: public domov, public garaz
+{
+public:
+	centrala();
+	~centrala();
+	void zamknivse();
+	void zamknigaraz();
+	void zamknidum();
+	void odemkni(d_budova budova);
+	void pridejsenzor(d_typsenzor senzor, d_budova misto);
+	void smazsenzor();
+
+
+private:
+
+};
--- Zabezpecovacka/senzor.h
+++ Zabezpecovacka/senzor.h
@@ -1,1 +1,111 @@
 #pragma once
+
+class senzor
+{
+public:
+	senzor(char* misto, char* typ);
+	senzor();
+	~senzor();
+	bool je_funkcni();
+	bool je_poplach();
+	virtual void jakysenzor();
+
+protected:
+	char* umisteni;
+	bool poplach;
+	bool funkcni;
+};
+
+senzor::senzor()
+{
+	poplach = false;
+	funkcni = true;
+}
+senzor::senzor(char* misto, char* typ)
+{
+	umisteni = misto;
+	poplach = false;
+	funkcni = true;
+}
+
+senzor::~senzor()
+{
+}
+
+bool senzor::je_funkcni()
+{
+	return funkcni;
+}
+bool senzor::je_poplach()
+{
+	return poplach;
+}
+void senzor::jakysenzor()
+{
+	cout << "Kontaktni senzor";
+}
+
+class senzorPohybovy: public senzor
+{
+public:
+	senzorPohybovy();
+	~senzorPohybovy();
+
+	void jakysenzor();
+
+private:
+
+};
+
+senzorPohybovy::senzorPohybovy()
+{
+}
+
+senzorPohybovy::~senzorPohybovy()
+{
+}
+
+void senzorPohybovy::jakysenzor()
+{
+	cout << "senzor pohybovy";
+}
+
+class senzorPozarni : public senzor
+{
+public:
+	senzorPozarni();
+	~senzorPozarni();
+
+private:
+	int teplota;
+
+};
+
+senzorPozarni::senzorPozarni()
+{
+	teplota = 22;
+}
+
+senzorPozarni::~senzorPozarni()
+{
+}
+
+class co2senzor: public senzor
+{
+public:
+	co2senzor();
+	~co2senzor();
+
+private:
+	int co2;
+
+};
+
+co2senzor::co2senzor()
+{
+	co2 = 5000;
+}
+
+co2senzor::~co2senzor()
+{
+}
